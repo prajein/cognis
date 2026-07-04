df797339423bf555bbc3314a0d276531c6078934
riya-5663 <riyachauhan2023@gmail.com>
2026-07-04 18:57:12 +0530

    feat: implement Surface B component architecture

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,8 @@
 // TODO: Add vite configuration
-export default {};
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
